480396aa4277ea70cf5d9a47049de7078c542e20
batamsieuhang <doanhtuhd123@gmail.com>
2023-05-05 16:22:06 +0700

    create gateway

---
+++ AMQP-gateway/src/main/java/com/network/programming/amqpgateway/AmqpGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.network.programming.amqpgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmqpGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AmqpGatewayApplication.class, args);
+    }
+
+}
