414b32bcfeaa1476bf7e76fcd7f9fac76a3b4c71
batamsieuhang <20021460@vnu.edu.vn>
2023-05-06 12:21:06 +0700

    add config db

--- mongo-entrypoint/entrypoint.js
+++ mongo-entrypoint/entrypoint.js
@@ -1,4 +1,4 @@
-var db = connect("mongodb://root:example@localhost:27017/root");
+var db = connect("mongodb://admin:pass@localhost:27017/admin");
 
 db = db.getSiblingDB('iot_data'); // we can not use "use" statement here to switch db
 
