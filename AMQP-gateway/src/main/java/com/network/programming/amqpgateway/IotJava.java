c297ad7898a544310f67d65dc4feb96086575c5b
batamsieuhang <20021460@vnu.edu.vn>
2023-05-08 03:32:29 +0700

    add iot data class

--- AMQP-gateway/src/main/java/com/network/programming/amqpgateway/IotJava.java
+++ AMQP-gateway/src/main/java/com/network/programming/amqpgateway/IotJava.java
@@ -11,15 +11,86 @@ import java.util.Date;
 public class IotJava {
 
     @MongoId
-    private int id;
+    private String id;
 
     @DateTimeFormat
-    private Date unixTime;
+    private Date iotDate;
+
+    private long unixTime;
     private float watValue;
+
     private int workLoad;
     private int plugId;
     private int houseHoldId;
     private int houseId;
+
+    public IotJava(Date iotDate, long unixTime, float watValue, int workLoad, int plugId, int houseHoldId, int houseId) {
+        this.iotDate = iotDate;
+        this.unixTime = unixTime;
+        this.watValue = watValue;
+        this.workLoad = workLoad;
+        this.plugId = plugId;
+        this.houseHoldId = houseHoldId;
+        this.houseId = houseId;
+    }
+
+    public Date getIotDate() {
+        return iotDate;
+    }
+
+    public void setIotDate(Date iotDate) {
+        this.iotDate = iotDate;
+    }
+
+    public long getUnixTime() {
+        return unixTime;
+    }
+
+    public void setUnixTime(long unixTime) {
+        this.unixTime = unixTime;
+    }
+
+    public float getWatValue() {
+        return watValue;
+    }
+
+    public void setWatValue(float watValue) {
+        this.watValue = watValue;
+    }
+
+    public int getWorkLoad() {
+        return workLoad;
+    }
+
+    public void setWorkLoad(int workLoad) {
+        this.workLoad = workLoad;
+    }
+
+    public int getPlugId() {
+        return plugId;
+    }
+
+    public void setPlugId(int plugId) {
+        this.plugId = plugId;
+    }
+
+    public int getHouseHoldId() {
+        return houseHoldId;
+    }
+
+    public void setHouseHoldId(int houseHoldId) {
+        this.houseHoldId = houseHoldId;
+    }
+
+    public int getHouseId() {
+        return houseId;
+    }
+
+    public void setHouseId(int houseId) {
+        this.houseId = houseId;
+    }
+
+
 
 
 }
