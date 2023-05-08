04f9be627060515e425c8b94ad19e4fe73aae456
batamsieuhang <20021460@vnu.edu.vn>
2023-05-09 02:30:28 +0700

    fix bug can't import db

--- AMQP-gateway/src/main/java/com/network/programming/amqpgateway/IotDataRepository.java
+++ AMQP-gateway/src/main/java/com/network/programming/amqpgateway/IotDataRepository.java
@@ -1,8 +1,11 @@
 package com.network.programming.amqpgateway;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
-
+@EnableMongoRepositories
 public interface IotDataRepository extends MongoRepository<IotJava, String> {
+
 
 }
