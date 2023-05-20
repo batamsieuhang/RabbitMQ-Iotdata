456ff6b13d826505a80fd1914678b030f9466ccb
mr8-ubuntu <20021460@vnu.edu.vn>
2023-05-20 21:20:09 +0700

    create censor data

---
+++ AMQP-gateway/src/main/java/com/network/programming/amqpgateway/Censor.java
@@ -0,0 +1,56 @@
+package com.network.programming.amqpgateway;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "censor")
+public class Censor{
+
+    @Id
+    String id;
+
+
+    int name;
+    String status;
+
+    String have_turn_on;
+
+    public String getHave_turn_on() {
+        return have_turn_on;
+    }
+
+    public void setHave_turn_on(String have_turn_on) {
+        this.have_turn_on = have_turn_on;
+    }
+
+    public Censor(String id, int name, String status, String have_turn_on) {
+        this.id = ""+name;
+        this.name = name;
+        this.status = status;
+        this.have_turn_on = have_turn_on;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public int getName() {
+        return name;
+    }
+
+    public void setName(int name) {
+        this.name = name;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
