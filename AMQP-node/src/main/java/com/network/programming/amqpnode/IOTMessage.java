b8a4b0c55c615620d5547bb2852ffb927617f047
mr8-ubuntu <20021460@vnu.edu.vn>
2023-05-20 17:46:23 +0700

    accept controller from gateway

---
+++ AMQP-node/src/main/java/com/network/programming/amqpnode/IOTMessage.java
@@ -0,0 +1,63 @@
+package com.network.programming.amqpnode;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import org.springframework.amqp.core.AcknowledgeMode;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.core.MessageListener;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.w3c.dom.Node;
+
+import java.io.IOException;
+import java.util.List;
+
+
+@Service
+public class IOTMessage implements MessageListener {
+
+
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+
+    @Override
+    public void onMessage(Message message) {
+        NodeCensor nodeCensor = new NodeCensor();
+        String msg = new String(message.getBody());
+        System.out.println(msg);
+        if (msg.startsWith("Enable ")) {
+            int plugId = Integer.parseInt(msg.substring(7));
+            try {
+                nodeCensor.processData(rabbitTemplate, plugId);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+
+
+
+
+
+
+
+    @Override
+    public void containerAckMode(AcknowledgeMode mode) {
+        MessageListener.super.containerAckMode(mode);
+    }
+
+    @Override
+    public boolean isAsyncReplies() {
+        return MessageListener.super.isAsyncReplies();
+    }
+
+    @Override
+    public void onMessageBatch(List<Message> messages) {
+        MessageListener.super.onMessageBatch(messages);
+    }
+
+
+}
